3501782dae76d681fdc2425effef041940b08a51
Suyel Bishnu <suyel2007@gmail.com>
2024-08-24 11:41:49 +0300

    Initial commit

---
+++ src/routes/Home.jsx
@@ -0,0 +1,27 @@
+import { Typography } from "@mui/material";
+import ComponentA from "../components/ComponentA";
+import useData from "../contexts/useData";
+
+// const data = await getData(url);
+
+// this is outside the component
+const Home = () => {
+  //   const [data, setData] = useState(null);
+  const { data } = useData();
+
+  console.log("Data: ", data);
+  // This is inside the component
+
+  return (
+    <div>
+      <Typography variant="h6" component="h2">
+        Home
+      </Typography>
+      <ComponentA />
+    </div>
+  );
+};
+
+// This is outside the component
+
+export default Home;
